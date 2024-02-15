72b0376a6ada8b8e75b0d82f679cfd83838d8556
zheka <talipov.zhenis@gmail.com>
2024-02-15 21:56:55 +0600

    database init

---
+++ cmd/web/config.go
@@ -0,0 +1,5 @@
+package main
+
+const (
+	StoragePath = "./storage/storage.db"
+)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,20 @@
 package main
 
+import (
+	"forum/internal/repository/sqlite"
+	"log"
+)
+
 func main() {
 	// init logger
 
-	// init db 
+	// init db
+	storage, err := sqlite.New(StoragePath)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = storage
 	// init router
 
 	// start server
